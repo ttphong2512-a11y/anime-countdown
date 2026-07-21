452b8b9d453ab48c7568d13b0ac099a64627c8df
ttphong2512-a11y <ttphong2512@gmail.com>
2026-07-21 10:58:45 +0700

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,1 +1,21 @@
-console.log("Anime Countdown Loaded");
+(function(){
+
+const box = document.getElementById("anime-countdown");
+
+if(!box) return;
+
+box.innerHTML = `
+<div style="
+background:#171717;
+padding:20px;
+border-radius:16px;
+color:white;
+font-family:Arial;
+text-align:center;
+">
+<h2>⏳ Anime Countdown</h2>
+<p>Đang kết nối dữ liệu...</p>
+</div>
+`;
+
+})();
